4fcd51dc8924b025a011b09936816c6a0b335454
SarahReyes <siwreyes@gmail.com>
2016-10-27 08:00:56 -0500

    in the middle of the comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,100 @@
-// this will hold the arrays?
-// I think we put the arrays of people who enter their info here
 
-// user fills in the form
 
-// grab the value of the user inputs
+var totalDifference = 0;
 
-// push the user input to an array of objects
+// hard code some 'friends' to compare with the user
+var friends = [
+    {
+        name: 'Pam',
+        photo: 'images/pam-pic.jpg',
+        scores: {
+            qone: 5,
+            qtwo: 4,
+            qthree: 5,
+            qfour: 5,
+            qfive: 5,
+            qsix: 1,
+            qseven: 5,
+            qeight: 5,
+            qnine: 1,
+            qten: 5,
+        }
+    },
+    {
+        name: 'Dwight',
+        photo: 'images/dwight-pic.jpg',
+        scores: {
+            qone: 1,
+            qtwo: 5,
+            qthree: 4,
+            qfour: 1,
+            qfive: 5,
+            qsix: 2,
+            qseven: 1,
+            qeight: 2,
+            qnine: 5,
+            qten: 1,
+        }
+    },
+    {
+        name: 'Stanley',
+        photo: 'images/stanley-pic.jpg',
+        scores: {
+            qone: 3,
+            qtwo: 5,
+            qthree: 1,
+            qfour: 2,
+            qfive: 2,
+            qsix: 5,
+            qseven: 3,
+            qeight: 1,
+            qnine: 2,
+            qten: 1,
+        }
+    }
+];
 
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//      5,
-//      1,
-//      4,
-//      4,
-//      5,
-//      1,
-//      2,
-//      5,
-//      4,
-//      1
-//    ]
-// }
 
 $('#surveySubmitBtn').on('click', function(){
+    // grab the values of the user inputs
     var newFriend = {
         name: $('#friend-name').val().trim(),
         photo: $('#friend-image').val().trim(),
-        scores: [$('#questionOne').val(), $('#questionTwo').val(), $('#questionThree').val(), $('#questionFour').val(), $('#questionFive').val(),
-                 $('#questionSix').val(), $('#questionSeven').val(), $('#questionEight').val(), $('#questionNine').val(), $('#questionTen').val()]
+        // put 'scores into an object and name each one qone, qtwo, etc' exactly like above
+
+        scores: {
+                    qone: $('#questionOne').val(),
+                    qtwo: $('#questionTwo').val(),
+                    qthree: $('#questionThree').val(),
+                    qfour: $('#questionFour').val(),
+                    qfive: $('#questionFive').val(),
+                    qsix: $('#questionSix').val(),
+                    qseven: $('#questionSeven').val(),
+                    qeight: $('#questionEight').val(),
+                    qnine: $('#questionNine').val(),
+                    qten: $('#questionTen').val()
+                }
     };
     // testing inputs with button click
-    console.log(newFriend.scores);
+    console.log(JSON.stringify(newFriend));
+
+
+    friends.forEach(function(friend){
+        for (var prop in friend.scores) {
+            console.log("This is the key: " + prop + " This is the value: " + friend.scores[prop]);
+            console.log(newFriend.scores[prop]);
+        }
+    });
+
+
 });
+
+// to compare
+// loop through their scores for all the friends in var 'friends'
+// compare the score var friends, against the one you got back
+// var totalDiff = 0;
+// friends.each do |friend|
+//   friend.scores.each do |key, value|
+//     totalDiff = totalDiff + Math.abs(newFriend.scores[key] - value)
+//   end
+//end
